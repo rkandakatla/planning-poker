f6ee5a6f67e67eb42c423d97c4ad98994f3cc6d3
RajKandakatla+1-at-843359312476 <rkandakatla@cleaverbrooks.com>
2022-03-11 15:08:30 +0000

    poker estimate changes

--- src/views/planningPoker.tsx
+++ src/views/planningPoker.tsx
@@ -6,11 +6,11 @@ const EXTENSION_ID = 'aha-develop.planning-poker';
 const FIELD_BASE = 'estimate';
 const ESTIMATES = {
   '0': { color: '#666666', backgroundColor: '#f1f1f1' },
-  '1': { color: '#326601', backgroundColor: '#c7dbaf' },
-  '2': { color: '#301c42', backgroundColor: '#e5dced' },
-  '3': { color: '#7d630b', backgroundColor: '#faebb9' },
-  '5': { color: '#c76d00', backgroundColor: '#fcddb8' },
-  '8': { color: '#992e0b', backgroundColor: '#fac0af' },
+  '2': { color: '#326601', backgroundColor: '#c7dbaf' },
+  '4': { color: '#301c42', backgroundColor: '#e5dced' },
+  '6': { color: '#7d630b', backgroundColor: '#faebb9' },
+  '8': { color: '#c76d00', backgroundColor: '#fcddb8' },
+  '10': { color: '#992e0b', backgroundColor: '#fac0af' },
 };
 const ESTIMATE_VALUES = Object.keys(ESTIMATES);
 
